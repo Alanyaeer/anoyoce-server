fa1ff9735b1febdde847e419f8b9dd75f11900a7
Alan <1563085328@qq.com>
2024-04-20 20:49:24 +0800

    create init

---
+++ src/main/java/com/lytech/anoyoce/AnoyoceApplication.java
@@ -0,0 +1,13 @@
+package com.lytech.anoyoce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnoyoceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnoyoceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lytech/anoyoce/AnoyoceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lytech.anoyoce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnoyoceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
