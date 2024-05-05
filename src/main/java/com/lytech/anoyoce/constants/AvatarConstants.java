269ccd8f7d397405cae03f823d5052c2ed2d3573
Alan <1563085328@qq.com>
2024-05-05 23:32:30 +0800

    add queryRoomId and get info

---
+++ src/main/java/com/lytech/anoyoce/constants/AvatarConstants.java
@@ -0,0 +1,5 @@
+package com.lytech.anoyoce.constants;
+
+public class AvatarConstants {
+    public static String avatar1 = "https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/3ee5f13fb09879ecb5185e440cef6eb9.png~tplv-uwbnlip3yd-webp.webp";
+}
